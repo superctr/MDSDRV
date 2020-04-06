27d3ef977f9b7d3bfef563412382eb810ac01e59
Ian Karlsson <themvrules@gmail.com>
2020-04-07 01:59:49 +0200

    #9 Add dummy Z80 driver

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
+tools\sjasmplus.exe mdssub.z80 --raw=mdssub.bin --lst=mdssub.lst >error.txt
+if %ERRORLEVEL% neq 0 goto error
 tools\asm68k.exe /k /p /o ae- main.68k, main.bin >error.txt, , main.lst
 if %ERRORLEVEL% neq 0 goto error
 tools\asm68k.exe /k /p /o ae- blob.68k, mdsdrv.bin >error.txt, , mdsdrv.lst
