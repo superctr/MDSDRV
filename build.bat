2ece95352bae07fd076d62138a8150a7424c0193
Ian Karlsson <themvrules@gmail.com>
2020-10-10 13:50:27 +0200

    Clean up file structure

--- build.bat
+++ build.bat
@@ -1,45 +1,43 @@
 @echo off
 rem build with Wine: wine cmd /c build.bat
+mkdir out
 echo Make mdsseq.bin and mdspcm.bin ...
-tools\mdslink.exe -o mdsseq.bin mdspcm.bin -i seqlist.inc -h seqlist.h ^
-  "sample/jazzy_nyc_99.mml" ^
-  "sample/idk.mds" ^
-  "sample/sand_light.mds" ^
-  "sample/junkers_high.mds" ^
-  "sample/midnight.mds" ^
-  "sample/astronomia.mds" ^
-  "sample/passport.mds" ^
-  "sample/pcmtest2.mds" ^
-  "sample/pcmtest.mds" ^
-  "sample/effect/beep1.mml" ^
-  "sample/effect/beep2.mml" ^
-  "sample/effect/beep3.mml" ^
-  "sample/effect/beep4.mml" ^
-  "sample/effect/explosion1.mml" ^
-  "sample/effect/explosion2.mml" ^
-  "sample/effect/explosion3.mml" ^
-  "sample/effect/menu1.mml" ^
-  "sample/effect/menu2.mml" ^
-  "sample/effect/menu3.mml" ^
-  "sample/effect/noise1.mml" ^
-  "sample/effect/noise2.mml" ^
-  > error.txt
+tools\mdslink.exe -o "out/mdsseq.bin" "out/mdspcm.bin" -i "out/mdsseq.inc" -h "out/mdsseq.h" ^
+  "data/bgm/jazzy_nyc_99.mml" ^
+  "data/bgm/idk.mds" ^
+  "data/bgm/sand_light.mds" ^
+  "data/bgm/junkers_high.mds" ^
+  "data/bgm/midnight.mds" ^
+  "data/bgm/passport.mds" ^
+  "data/se/beep1.mml" ^
+  "data/se/beep2.mml" ^
+  "data/se/beep3.mml" ^
+  "data/se/beep4.mml" ^
+  "data/se/explosion1.mml" ^
+  "data/se/explosion2.mml" ^
+  "data/se/explosion3.mml" ^
+  "data/se/menu1.mml" ^
+  "data/se/menu2.mml" ^
+  "data/se/menu3.mml" ^
+  "data/se/noise1.mml" ^
+  "data/se/noise2.mml" ^
+  > out\error.txt
 if %ERRORLEVEL% neq 0 goto error
 echo Make mdssub.bin ...
-tools\sjasmplus.exe mdssub.z80 --raw=mdssub.bin --lst=mdssub.lst >error.txt
+tools\sjasmplus.exe src\mdssub.z80 --raw=out\mdssub.bin --lst=out\mdssub.lst >out\error.txt
 if %ERRORLEVEL% neq 0 goto error
 echo Make mdssub.slz ...
-tools\slz.exe -c mdssub.bin mdssub.slz
+tools\slz.exe -c out\mdssub.bin out\mdssub.slz
 if %ERRORLEVEL% neq 0 goto error
 echo Make main.bin ...
-tools\asm68k.exe /k /p /o ae- main.68k, main.bin >error.txt, , main.lst
+tools\asm68k.exe /k /p /o ae- src\main.68k, main.bin >out\error.txt, , out\main.lst
 if %ERRORLEVEL% neq 0 goto error
 echo Make mdsdrv.bin ...
-tools\asm68k.exe /k /p /o ae- blob.68k, mdsdrv.bin >error.txt, , mdsdrv.lst
+tools\asm68k.exe /k /p /o ae- src\blob.68k, out\mdsdrv.bin >out\error.txt, , out\mdsdrv.lst
 if %ERRORLEVEL% neq 0 goto error
 echo All done!
 goto eof
 :error
-type error.txt
+type out\error.txt
 pause
 :eof
